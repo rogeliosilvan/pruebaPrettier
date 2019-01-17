279263b516fd109e2ae9e1d1b8a3dd1b9de7d7db
rogesilvan <rogoffspring@gmail.com>
2019-01-17 15:31:21 +0100

    primer commit pruebaPrettier

---
+++ src/index.js
@@ -0,0 +1,7 @@
+ import './style/main.scss'
+
+
+const arr = [1, 2, 3]
+const iAmJavascriptES6 = () => arr
+
+console.log(iAmJavascriptES6)
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+       }
+  ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ]
+};
+
